79d4d73a69ecbb6d1d45465b0f2726dde20dd961
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-02-22 20:50:53 +0200

    main project

---
+++ src/assets/js/languages.js
@@ -0,0 +1,20 @@
+export const languages = {
+  en: {
+    editions: "Editions",
+    controller: "Controller",
+    about: "About",
+    explore: "Explore",
+    news: "News",
+    faq: "FAQ",
+    trailer: "Watch the Trailer",
+  },
+  uk: {
+    editions: "Видання",
+    controller: "Джойстик",
+    about: "Про гру",
+    explore: "Досліджувати",
+    news: "Новини",
+    faq: "Часті запитання",
+    trailer: "Переглянути трейлер",
+  },
+};
