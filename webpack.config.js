d91203276d138fb65f64914cdc1597131de40377
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-02-23 12:02:39 +0200

    fix dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     clean: true,
     filename: "[name].bundle.js", // [name] тут дорівнює app
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
   devServer: {
